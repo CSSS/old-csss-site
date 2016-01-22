ff6012301b55ed6cf833ae06d21c99e1d84fdafa
asabaya3 <asabaya3@gmail.com>
2016-01-21 20:52:16 -0800

    Add disqus integration

--- cms/urls.py
+++ cms/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.AnnouncementListView.as_view(), name='index'),
+    url(r'^announcement/(?P<slug>[-\w]+)/$', views.AnnouncementDetailView.as_view(), name='announcement'),
     url(r'^(?P<slug>[-\w]+)/$', views.PostListView.as_view(), name='posts_in_category'),
     url(r'^([-\w]+)/(?P<slug>[-\w]+)/$', views.PostDetailView.as_view(), name='post'),
 ]
